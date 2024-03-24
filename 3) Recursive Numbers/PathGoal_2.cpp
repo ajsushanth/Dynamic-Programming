870aae2ff5c11f3f9991c26995e66678fce87ac3
ajsushanth <ajsushanth@gmail.com>
2024-03-24 15:29:43 +0530

    if-else statements reduced

--- 3) Recursive Numbers/PathGoal_2.cpp
+++ 3) Recursive Numbers/PathGoal_2.cpp
@@ -11,19 +11,13 @@ An obstacle is marked as 1, and an unoccupied space is marked as 0 in the matrix
 using namespace std;
 
 int uniquePath(vector<vector<int>>& matrix, int i, int m, int j, int n, vector<vector<int>>& path){
-    if(i == m || j == n) return 0;
-
-    if(matrix[i][j] == 1) return 0;
+    if(i == m || j == n || matrix[i][j] == 1) return 0;
 
     if(i == m-1 && j == n-1) return 1;
 
     if(path[i][j] != -1) return path[i][j];
 
-    if(i < m-1 && j < n-1) path[i][j] = uniquePath(matrix, i+1, m, j, n, path) + uniquePath(matrix, i, m, j+1, n, path);
-
-    else if(i < m-1) path[i][j] = uniquePath(matrix, i+1, m, j, n, path);
-
-    else path[i][j] = uniquePath(matrix, i, m, j+1, n, path);
+    path[i][j] = uniquePath(matrix, i+1, m, j, n, path) + uniquePath(matrix, i, m, j+1, n, path);
 
     return path[i][j];
 }
